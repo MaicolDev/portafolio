840c80d6e11d3dad47d3c59e7921aa2c8d0376cd
Maicol <maicolfranco800@gmail.com>
2023-03-04 16:22:21 -0300

    arreglando estaticos 3

--- app/settings.py
+++ app/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/media/'
+STATIC_URL = 'media'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 
